5684503158359f0f8f00df2b29f8633d032fd604
Jackson P <jacksonpauls@gmail.com>
2015-04-03 09:43:42 +0100

    Use device.uuid for a person's id if present instead of generating our own uuid.

--- js/person.js
+++ js/person.js
@@ -12,10 +12,15 @@ function Person (name, id, isMe) {
     if (localStorage.getItem('person_id')) {
       self.id = localStorage.getItem('person_id');
     } else {
-      self.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) { // http://stackoverflow.com/a/2117523
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-      });
+      if (typeof device === 'undefined') {
+        self.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) { // http://stackoverflow.com/a/2117523
+          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
+      } else {
+        // The uuid on iOS is not unique to a device, but varies for each application, for each installation. It changes if you delete and re-install the app, and possibly also when you upgrade iOS, or even upgrade the app per version (apparent in iOS 5.1). The uuid is not a reliable value.
+        self.id = device.uuid;
+      }
       localStorage.setItem('person_id', self.id);
     }
     self.is_me = true;
