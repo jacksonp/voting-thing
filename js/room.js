fbd58276ed9225cb4f4f38327d2a9b437de655ba
Jackson P <jacksonpauls@gmail.com>
2015-04-17 09:13:03 +0100

    Bump version for new release + fix padding in setup form.

--- js/room.js
+++ js/room.js
@@ -44,7 +44,7 @@ function RoomViewModel (socket, setupDoneCB, toast) {
   function myEmit (action, extraData) {
     extraData = extraData || {};
     socket.emit(action, $.extend(extraData, {
-      v        : '0.4.0',
+      v        : '0.4.1',
       room     : self.room(),
       person_id: self.people.me.id,
       name     : self.people.me.name()
