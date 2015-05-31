beaa30b99e14a442edaac03cabf548353c29b252
Jackson P <jacksonpauls@gmail.com>
2015-05-31 16:12:23 +0100

    Version bump to 0.5.2.

--- js/room.js
+++ js/room.js
@@ -206,7 +206,7 @@ function RoomViewModel () {
   function myEmit (action, extraData) {
     extraData = extraData || {};
     var data = $.extend(extraData, {
-      v        : '0.5.1',
+      v        : '0.5.2',
       action   : action,
       room     : self.room(),
       person_id: self.people.me.id,
