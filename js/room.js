9af33aa0995bc37608d0bac05edd3fe96cb987e2
Jackson P <jacksonpauls@gmail.com>
2016-09-30 09:38:28 +0100

    General tidy.

--- js/room.js
+++ js/room.js
@@ -59,7 +59,6 @@ function RoomViewModel () {
   function connect () {
 
     socket = new WebSocket('ws://votingthing.com:3883/');
-    //socket = new WebSocket('ws://192.168.1.69:3883/');
 
     socket.onopen = function () {
       self.sync();
